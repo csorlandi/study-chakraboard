f0dec6f2a6b7464495f18191fe5bad0fe30237ee
Claudio Orlandi <claudiosorlandi@gmail.com>
2022-08-03 15:34:15 -0300

    :sparkles: feat: create active link component for sidebar links

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -4,8 +4,8 @@ import {
   Text,
   LinkProps as ChakraLinkProps,
 } from '@chakra-ui/react';
-import Link from 'next/link';
 import { ElementType } from 'react';
+import { ActiveLink } from '../ActiveLink';
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
@@ -15,13 +15,13 @@ interface NavLinkProps extends ChakraLinkProps {
 
 export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" alignItems="center" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">
           {children}
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 }
