8304b0bb84c3102df738478372005263f07c0da3
Claudio Orlandi <claudiosorlandi@gmail.com>
2022-08-03 13:41:51 -0300

    :lipstick: feat: apply responsive styles on header

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,7 +2,7 @@ import { Text } from '@chakra-ui/react';
 
 export function Logo() {
   return (
-    <Text fontSize="3xl" fontWeight="bold" w="64">
+    <Text fontSize={['2xl', '3xl']} fontWeight="bold" w="64">
       Chakraboard
       <Text as="span" color="pink.500" ml="1">
         .
--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Claudio Orlandi</Text>
-        <Text color="gray.300" fontSize="small">
-          claudiosorlandi@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Claudio Orlandi</Text>
+          <Text color="gray.300" fontSize="small">
+            claudiosorlandi@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
