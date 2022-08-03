b0ff685e476b37e765629e2affdc46034d59c276
Claudio Orlandi <claudiosorlandi@gmail.com>
2022-08-03 01:13:59 -0300

    :sparkles: feat: add header componente on dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import { Flex } from '@chakra-ui/react';
+import { Header } from '../components/Header';
+
+export default function Dashboard() {
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        Sidebar
+      </Flex>
+    </Flex>
+  );
+}
