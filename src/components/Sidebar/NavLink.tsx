cec4bd197c232baff3c0c8cc48a9711f36246364
Claudio Orlandi <claudiosorlandi@gmail.com>
2022-08-03 12:09:14 -0300

    :recycle: refactor: break sidebar into small components

---
+++ src/components/Sidebar/NavLink.tsx
@@ -0,0 +1,23 @@
+import {
+  Icon,
+  Link,
+  Text,
+  LinkProps as ChakraLinkProps,
+} from '@chakra-ui/react';
+import { ElementType } from 'react';
+
+interface NavLinkProps extends ChakraLinkProps {
+  icon: ElementType;
+  children: string;
+}
+
+export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+  return (
+    <Link display="flex" alignItems="center" {...rest}>
+      <Icon as={icon} fontSize="20" />
+      <Text ml="4" fontWeight="medium">
+        {children}
+      </Text>
+    </Link>
+  );
+}
